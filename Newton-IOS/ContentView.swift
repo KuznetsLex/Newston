e33586d9a37ba3ca8baeb0b63b9d335cfa43abac
KuznetsLex <kuznets.lex@gmail.com>
2022-07-20 16:37:04 +0600

    Init project

---
+++ Newton-IOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Newton-IOS
+//
+//  Created by test on 20.07.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Newton-IOS/Newton_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Newton_IOSApp.swift
+//  Newton-IOS
+//
+//  Created by test on 20.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct Newton_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
